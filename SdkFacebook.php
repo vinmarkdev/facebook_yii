ad1bd1795a5bdd02511e3e9b2910b114ee34b2bc
Mark Mamchur <vinexx1@mail.ru>
2017-09-14 23:31:28 +0300

    new project Facebook SDK Account

---
+++ SdkFacebook.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace vinmarkdev\facebook_yii;
+
+use Yii;
+use yii\base\Component;
+use Exception;
+
+use Facebook\Facebook;
+
+class SdkFacebook extends Component{
+
+    /*public $app_id;
+    public $app_secret;
+    public $default_graph_version;
+
+    public function __construct()
+    {
+        parent::__construct([
+            'app_id' => $this->app_id,
+            'app_secret' => $this->app_secret,
+            'default_graph_version' => $this->default_graph_version
+        ]);
+    }*/
+    
+}
